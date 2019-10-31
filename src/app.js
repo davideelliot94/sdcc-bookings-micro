c33fdd9c59a8c5265b1f12b39164836822ca2621
davide_elliot <33093208+davideelliot94@users.noreply.github.com>
2019-10-31 13:32:41 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Room = require("./models/rooms_model");
 const Booking = require("./models/booking_model");
+const Teaching = require("./models/teaching_model");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -8,24 +9,36 @@ const cors = require("cors");
 app.use(cors());
 app.use(bodyParser.json());
 
+/**
+ *  PRESENTATION
+*/
 app.get("/", (req, res) => {
     res.json({ msg: "I'm booking service, I'm up!" });
 });
 
-app.post("/api/v1/rooms", async (req, res) => {
+/**
+ *  ROOMS
+*/
+
+app.get("/api/v1/rooms", async (req, res) => {
     const rooms = await Room.find({});
     res.json(rooms);
 });
 
-app.get("/api/v1/roomsById", async (req, res) => {
-    const rooms = await Room.findOne({ "id": req.body.stringify()});
+app.post("/api/v1/roomsById", async (req, res) => {
+    var id = req.body.stringify();
+    const rooms = await Room.findById(id);
     res.json(rooms);
 });
 
-app.get("/api/v1/roomsByName", async (req, res) => {
+app.post("/api/v1/roomsByName", async (req, res) => {
     const rooms = await Room.findOne({ "name": req.body.stringify()});
     res.json(rooms);
 });
+
+/**
+ *  BOOKINGS
+*/
 
 app.get("/api/v1/bookings", async (req, res) => {
     const bookings = await Booking.find({});
@@ -33,7 +46,8 @@ app.get("/api/v1/bookings", async (req, res) => {
 });
 
 app.post("/api/v1/bookingsById", async (req, res) => {
-    const bookings = await Booking.findOne({ "id": req.body.stringify()});
+    id = req.body.stringify();
+    const bookings = await Booking.findById(id);
     res.json(bookings);
 });
 
@@ -61,7 +75,6 @@ app.post("/api/v1/bookingsByRoomAndDate", async (req, res) => {
 });
 */
 
-
   
 app.post("/api/v1/makeBooking", async (req, res) => {
     const booking = new Booking({ id: req.body.id,
@@ -77,3 +90,23 @@ app.post("/api/v1/makeBooking", async (req, res) => {
     const savedBooking = await booking.save();
     res.json("La prenotazione "+savedBooking.id + " è avvenuta con successo");
 });
+
+/**
+ *  TEACHING
+*/
+
+app.get("/api/v1/teachings", async (req, res) => {
+    const teachings = await Teaching.find({});
+    res.json(teachings);
+});
+
+app.post("/api/v1/teachingsById", async (req, res) => {
+
+    id = req.body.id.stringify();
+    const teaching = await Teaching.findById(id);
+    res.json(teaching);
+});
+
+
+module.exports = app;
+
