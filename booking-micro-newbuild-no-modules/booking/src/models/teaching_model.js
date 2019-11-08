22e50b8e88322beb643b43eb01120dfba721a38d
davide_elliot <33093208+davideelliot94@users.noreply.github.com>
2019-11-08 14:41:25 +0100

    Add files via upload

--- booking-micro-newbuild-no-modules/booking/src/models/teaching_model.js
+++ booking-micro-newbuild-no-modules/booking/src/models/teaching_model.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema;
 const TeachingSchema = new Schema({
     name: String,
     id_prof: Number,
-    id_studend: Number,    
+    id_studend: [{
+    		type: String
+	}],    
   });
   
   module.exports = mongoose.model("Teaching", TeachingSchema);
