400369ace79ea26525473e6518cc2527bb24565b
claudio <clo.santoro@hotmail.it>
2019-11-15 14:25:30 +0100

    msg

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const app = require("./src/app");
 const { DB_URI } = require("./src/config");
 const mongoose = require("mongoose");
-mongoose.connect(DB_URI);
+//mongoose.connect(DB_URI);
+
+/*mongoose.connect(DB_URI, function(err, db) {
+    if (err) {
+        console.log('Unable to connect to the server. Please start the server. Error:', err);
+    } else {
+        console.log('Connected to Server successfully!');
+    }
+});*/                           //MI DA ERRORE, FORSE PERCHÉ MI MANCA IL CONTAINER?
 
 //fai connessione a db
 
-app.listen(8080, () => {
+app.listen(3000, () => {
     console.log("running on port 8080");
     console.log("--------------------------");
 });
--- src/app.js
+++ src/app.js
@@ -1,17 +1,48 @@
-const express = require("express");
-const Room = require("./models/rooms_model");
-const Booking = require("./models/booking_model");
-const Teaching = require("./models/teaching_model");
-const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
+var express = require("express");
+var Room = require("./models/rooms_model");
+var Booking = require("./models/booking_model");
+var Teaching = require("./models/teaching_model");
+var app = express();
+var bodyParser = require("body-parser");
+var cors = require("cors");
+var jwt = require('jsonwebtoken');
+var verifyOptions = {
+    algorithm:  ["RS256"]
+};
+var kid = "6jqcZFFzaqP04u85tWTfAw55ls6PTbiABoDj/5I6LPQ=";
 app.use(cors());
 app.use(bodyParser.json());
 
 /**
  *  PRESENTATION
 */
+
+app.use(function (req, res, next) {
+    // do something with the request
+    var jwtToken;
+    console.log('testing');
+    var nweText = req.body;
+    console.log('body is: ' + JSON.stringify(nweText));
+
+    if(nweText !== null && nweText !== undefined) {
+        jwtToken = /*JSON.parse(nweText).jwtToken;*/nweText.jwtToken;
+        var jwtDecode = require('jwt-decode');
+        var decoded = jwtDecode(jwtToken);
+        console.log('decoded: ' + JSON.stringify(decoded));
+        var jwkToPem = require('jwk-to-pem');
+        var pem = jwkToPem(decoded);
+        jwt.verify(jwtToken, pem, { algorithms: ['RS256'] }, function(err, decodedToken) {
+        });
+        //var decoded = jwt.verify(jwtToken,kid);
+        console.log('token is: ' + JSON.stringify(jwtToken));
+    }
+
+    //console.log('nwe is: ' + nwe);
+    next(); // MUST call this or the routes will not be hit
+});
+
+
+
 app.get("/", (req, res) => {
     res.json({ msg: "I'm booking service, I'm up!" });
 });
@@ -55,29 +86,14 @@ app.post("/api/v1/bookingsByDate", async (req, res) => {
     const bookings = await Booking.find({ "date": req.body.date});
     res.json(bookings);
 });
-/*
-app.post("/api/v1/bookingsByRoomId", async (req, res) => {
-    const bookings = await Booking.find({ "Room": req.body.rid});
-    res.json(bookings);
-});
 
-app.post("/api/v1/bookingsByTeaching", async (req, res) => {
-    const bookings = await Booking.find({ "teaching": req.body.teach});
-    res.json(bookings);
-});
-
-
-app.post("/api/v1/bookingsByRoomAndDate", async (req, res) => {
-    const bookingByRoom = await Booking.find({ "RoomId": req.body.rid});
-    const bookings = await Booking.find({ "startDate": bookingByRoom.startDate,
-                                            "endDate": bookingByRoom.endDate});
-    res.json(bookings);
-});
-*/
 
   
 app.post("/api/v1/saveBooking", async (req, res) => {
-    const booking = new Booking({ id: req.body.id,
+    console.log('got request');
+    const booking = new Booking({
+        jwtToken: req.body.jwtToken,
+        id: req.body.id,
         timestamp: req.body.now, 
         date: req.body.date,
         start: req.body.start,
